045998b64966701b20e70556349613fad49483a6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-23 11:20:05 -0600

    Add cluster wait function

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from time import time
+from time import time, sleep
 import pandas as pd
 
 
@@ -17,3 +17,19 @@ class DiagnosticTimer:
 
     def dataframe(self):
         return pd.DataFrame(self.diagnostics)
+
+
+def cluster_wait(client, n_workers):
+    """ Delay process until all workers in the cluster are available.
+    """
+    start = time()
+    wait_thresh = 300
+    worker_thresh = n_workers * 0.95
+
+    while len(client.cluster.scheduler.workers) < n_workers:
+        sleep(2)
+        elapsed = time() - start
+        # If we are getting close to timeout but cluster is mostly available,
+        # just break out
+        if elapsed > wait_thresh and len(client.cluster.scheduler.workers) >= worker_thresh:
+            break
