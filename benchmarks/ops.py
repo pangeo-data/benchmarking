afcc7f75375fd6982a1d28a2a11ec435d786d11e
Tina Odaka <46813815+tinaok@users.noreply.github.com>
2019-08-25 23:43:00 +0200

    Updating idias for ops.py for adding IO bench

--- benchmarks/ops.py
+++ benchmarks/ops.py
@@ -1,6 +1,16 @@
+# def writedown_zarr
+# def writedown_netcdf( create yeary netcdf files)
+# def mean operations ( global, temporal) with reading netcdf data
+# def mean operations ( global , temporal) with reading zarr file
+
+
+
 def global_mean(ds):
     return ds.mean(dim='time')
-
+# add plots here?and save it as png or such? 
+# if we bench pangeo as interactive hoc post processing tool
+# creating plot = analysis 
+# should be added ?
 
 def temporal_mean(ds):
     return ds.mean(dim=['lat', 'lon'])
