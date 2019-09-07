5ff3e081adbbf650dbe20a63462b5affdc73bd01
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-09-07 06:57:08 -0600

    Fix pandas date out of range

--- benchmarks/datasets.py
+++ benchmarks/datasets.py
@@ -9,14 +9,14 @@ import dask
 
 def timeseries(
     chunk_per_worker=10,
-    chunk_size='128 MB',
+    chunk_size="128 MB",
     num_nodes=1,
     worker_per_node=1,
     chunking_scheme=None,
     lat=320,
     lon=384,
-    start='1980-01-01',
-    freq='1D',
+    start="1980-01-01",
+    freq="1H",
     nan=False,
 ):
     """ Create synthetic Xarray dataset filled with random
@@ -24,9 +24,11 @@ def timeseries(
 
     Parameters
     ----------
-    chunk_per_worker : int 
-          number of chunk placed per worker.  
-          see docs.dask.org, best practices, for chunk.  Best chunk size is around 100M but, each worker can have many chunk, which automate the parallelism in dask.  
+    chunk_per_worker : int
+          number of chunk placed per worker.
+          see docs.dask.org, best practices, for chunk.
+          Best chunk size is around 100M but, each worker can
+          have many chunk, which automate the parallelism in dask.
     chunk_size : str
           chunk size in bytes, kilo, mega or any factor of bytes
     num_nodes : int
@@ -35,7 +37,8 @@ def timeseries(
            number of dask workers per node
 
     chunking_scheme : str
-           Whether to chunk across time dimension ('temporal') or horizontal dimensions (lat, lon) ('spatial').
+           Whether to chunk across time dimension ('temporal') or
+           horizontal dimensions (lat, lon) ('spatial').
            If None, automatically determine chunk sizes along all dimensions.
 
     lat : int
@@ -66,45 +69,45 @@ def timeseries(
     * lon      (lon) float64 -180.0 -179.4 -178.8 -178.2 ... 178.8 179.4 180.0
     * lat      (lat) float64 -90.0 -89.64 -89.28 -88.92 ... 88.92 89.28 89.64 90.0
     Data variables:
-        sst      (time, lon, lat) float64 dask.array<shape=(267, 600, 500), chunksize=(267, 245, 245)>
+        sst      (time, lon, lat) float64 dask.array<shape=(267, 600, 500), .....
     Attributes:
         history:  created for compute benchmarking
     """
 
-    dt = np.dtype('f8')
+    dt = np.dtype("f8")
     itemsize = dt.itemsize
     chunk_size = parse_bytes(chunk_size)
     total_bytes = chunk_size * num_nodes * worker_per_node * chunk_per_worker
-    #total_bytes = chunk_size * num_nodes * worker_per_node 
+    # total_bytes = chunk_size * num_nodes * worker_per_node
     size = total_bytes / itemsize
     timesteps = math.ceil(size / (lat * lon))
     shape = (timesteps, lon, lat)
-    if chunking_scheme == 'temporal':
+    if chunking_scheme == "temporal":
         x = math.ceil(chunk_size / (lon * lat * itemsize))
         chunks = (x, lon, lat)
-    elif chunking_scheme == 'spatial':
+    elif chunking_scheme == "spatial":
         x = math.ceil(math.sqrt(chunk_size / (timesteps * itemsize)))
         chunks = (timesteps, x, x)
     else:
-        chunks = 'auto'
+        chunks = "auto"
 
-    lats = xr.DataArray(np.linspace(start=-90, stop=90, num=lat), dims=['lat'])
-    lons = xr.DataArray(np.linspace(start=-180, stop=180, num=lon), dims=['lon'])
-    times = xr.DataArray(pd.date_range(start=start, freq=freq, periods=timesteps), dims=['time'])
-    if chunks == 'auto':
-        with dask.config.set({'array.chunk-size': chunk_size}):
+    lats = xr.DataArray(np.linspace(start=-90, stop=90, num=lat), dims=["lat"])
+    lons = xr.DataArray(np.linspace(start=-180, stop=180, num=lon), dims=["lon"])
+    times = xr.DataArray(pd.date_range(start=start, freq=freq, periods=timesteps), dims=["time"])
+    if chunks == "auto":
+        with dask.config.set({"array.chunk-size": chunk_size}):
             random_data = randn(shape=shape, chunks=chunks, nan=nan)
     else:
         random_data = randn(shape=shape, chunks=chunks, nan=nan)
     ds = xr.DataArray(
         random_data,
-        dims=['time', 'lon', 'lat'],
-        coords={'time': times, 'lon': lons, 'lat': lats},
-        name='sst',
+        dims=["time", "lon", "lat"],
+        coords={"time": times, "lon": lons, "lat": lats},
+        name="sst",
         encoding=None,
-        attrs={'units': 'baz units', 'description': 'a description'},
+        attrs={"units": "baz units", "description": "a description"},
     ).to_dataset()
-    ds.attrs = {'history': 'created for compute benchmarking'}
+    ds.attrs = {"history": "created for compute benchmarking"}
 
     return ds
 
