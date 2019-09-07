8b1026cf60980c91c9a453ab3c9ddaa4397b29ce
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-09-07 04:25:39 -0600

    Fixes #14, Fixes #15

--- benchmarks/ops.py
+++ benchmarks/ops.py
@@ -1,27 +1,22 @@
-# def writedown_zarr
-# def writedown_netcdf( create yeary netcdf files)
-# def mean operations ( global, temporal) with reading netcdf data
-# def mean operations ( global , temporal) with reading zarr file
+import numpy as np
 
-
-
-def global_mean(ds):
-    return ds.mean(dim='time')
-# add plots here?and save it as png or such? 
-# if we bench pangeo as interactive hoc post processing tool
-# creating plot = analysis 
-# should be added ?
 
 def temporal_mean(ds):
-    return ds.mean(dim=['lat', 'lon'])
+    return ds.mean(dim="time")
+
+
+def spatial_mean(ds):
+    weights = np.cos(np.radians(ds.lat)).where(ds["sst"][0].notnull())
+    weights /= weights.sum()
+    return (ds * weights).mean(dim=["lat", "lon"])
 
 
 def climatology(ds):
-    seasonal_clim = ds.groupby('time.season').mean(dim='time')
+    seasonal_clim = ds.groupby("time.season").mean(dim="time")
     return seasonal_clim
 
 
 def anomaly(ds):
     seasonal_clim = climatology(ds)
-    seasonal_anom = ds.groupby('time.season') - seasonal_clim
+    seasonal_anom = ds.groupby("time.season") - seasonal_clim
     return seasonal_anom
