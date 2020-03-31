b15bf6bcfbee37d5a000d9eb1dc0df1290940fc6
haiying xu <haiyingx@ucar.edu>
2020-03-31 10:34:45 -0600

    Make the repository pip installable (#40)
    * test
    
    * test again
    
    * see if upstream set
    
    * test hook with tab
    
    * reomve test_hook.py
    
    * remove test.md
    
    * add pip package setup.py
    
    * Update setup.py
    
    Co-Authored-By: Anderson Banihirwe <axbanihirwe@ualr.edu>
    
    * add install_req
    
    * Update setup.py
    
    Co-Authored-By: Kevin Paul <kpaul@ucar.edu>
    
    * Update setup.py
    
    Co-Authored-By: Kevin Paul <kpaul@ucar.edu>
    
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>
    Co-authored-by: Kevin Paul <kpaul@ucar.edu>

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pangeobench",
+    scripts=["pangeobench"],
+    author="Haiying Xu",
+    author_email="haiyingx@ucar.edu",
+    description="Pangeo zarr benchmarking package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    use_scm_version={"version_scheme": "post-release", "local_scheme": "dirty-tag"},
+    install_requires=["dask>=2.11", "xarray>=0.14", "numpy>1.17"],
+    url="https://github.com/pangeo-data/benchmarking",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
